752cbeb8b2459b545b822d8826f7f0578e613db5
김중연 <jjjr69@naver.com>
2022-02-14 17:05:11 +0900

    ....

---
+++ SensorMaker/SensorMaker/ClientApp/src/app.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './app.css';
+import AreaInformation from './components/titleChoice/AreaInformation.jsx';
+
+function App() {
+    return (
+        <AreaInformation />
+    );
+}
+export default App;
---
+++ SensorMaker/SensorMaker/ClientApp/src/components/titleChoice/AreaInformation.jsx
@@ -0,0 +1,41 @@
+﻿import React, { Component } from 'react';
+import { BsFillPlusCircleFill } from "react-icons/bs";
+import { BsCloudUpload } from "react-icons/bs";
+import { BsGear } from "react-icons/bs";
+import { FaSearch } from "react-icons/fa";
+import "./AreaInformation.css";
+
+class AreaInformation extends Component {
+    state = {
+        count: 0,
+    };
+    /*
+    handleIncrement = () => {
+        this.setState({ count: this.state.count + 1 });
+    }
+    */
+    render() {
+        return (
+            <>
+                <div class="header">
+                    <h2 class="title">공간정보<BsGear /></h2>
+                    <div className="search">
+                        <FaSearch  />
+                        <input type="text" className="" />
+                    </div>
+                    <button className="button1">
+                        <BsFillPlusCircleFill  />
+                    </button>
+                    <button className="button2">
+                        <BsCloudUpload />
+                    </button>
+                </div>
+                <body>
+
+                </body>
+            </>
+        );
+    };
+}
+
+export default AreaInformation;
